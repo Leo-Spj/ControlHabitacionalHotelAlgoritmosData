9e7077c2aa059b5012a54bf3d27d4aeb25332ff3
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-12-04 12:49:14 -0500

    legibilidad en ordenamiento

--- src/main/java/algoritmos/Ordenamiento.java
+++ src/main/java/algoritmos/Ordenamiento.java
@@ -5,43 +5,53 @@ import java.util.Comparator;
 public class Ordenamiento<T> {
 
     public ListaDoblePilaCola<T> porInsercion(ListaDoblePilaCola<T> lista, Comparator<T> criterio) {
+
         ListaDoblePilaCola<T> listaAuxiliar = new ListaDoblePilaCola<>();
+
         while (!lista.estaVacia()) {
+
             T dato = lista.getPrimero();
             lista.eliminarAlInicio();
+
             while (!listaAuxiliar.estaVacia() && criterio.compare(dato, listaAuxiliar.getUltimo()) < 0) {
                 lista.insertarAlInicio(listaAuxiliar.getUltimo());
                 listaAuxiliar.eliminarAlFinal();
             }
+
             listaAuxiliar.insertarAlFinal(dato);
         }
+
         while (!listaAuxiliar.estaVacia()) {
             lista.insertarAlInicio(listaAuxiliar.getUltimo());
             listaAuxiliar.eliminarAlFinal();
         }
+
         return lista;
     }
 
     public ListaDoblePilaCola<T> deBurbuja(ListaDoblePilaCola<T> lista, Comparator<T> criterio) {
+
         ListaDoblePilaCola<T> listaAuxiliar = new ListaDoblePilaCola<>();
+
         while (!lista.estaVacia()) {
+
             T dato = lista.getPrimero();
             lista.eliminarAlInicio();
+
             while (!listaAuxiliar.estaVacia() && criterio.compare(dato, listaAuxiliar.getUltimo()) < 0) {
                 lista.insertarAlInicio(listaAuxiliar.getUltimo());
                 listaAuxiliar.eliminarAlFinal();
             }
+
             listaAuxiliar.insertarAlFinal(dato);
         }
+
         while (!listaAuxiliar.estaVacia()) {
             lista.insertarAlInicio(listaAuxiliar.getUltimo());
             listaAuxiliar.eliminarAlFinal();
         }
+
         return lista;
     }
-
-
-
-
 
 }
